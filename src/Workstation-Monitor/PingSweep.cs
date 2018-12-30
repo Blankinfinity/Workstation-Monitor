0ee2d572fc61fb20380fc044f771bf8526b64b36
Keith Pearce <keith@keith-pearce.co.uk>
2018-12-30 11:21:41 +0000

    Moved code to separate class. Code based off of http://justinmklam.com/posts/2018/02/ping-sweeper/

---
+++ src/Workstation-Monitor/PingSweep.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Workstation_Monitor
+{
+    public class PingSweep
+    {
+        private string BaseIP = "192.168.1.";
+        private int StartIP = 1;
+        private int StopIP = 255;
+        private string ip;
+
+        private int timeout = 100;
+        private int nFound = 0;
+
+        static object lockObj = new object();
+        Stopwatch stopWatch = new Stopwatch();
+        TimeSpan ts;
+
+        public void RunPingSweep_Sync()
+        {
+            nFound = 0;
+
+            stopWatch.Start();
+            System.Net.NetworkInformation.Ping p = new System.Net.NetworkInformation.Ping();
+
+            for (int i = StartIP; i <= StopIP; i++)
+            {
+                ip = BaseIP + i.ToString();
+                System.Net.NetworkInformation.PingReply rep = p.Send(ip, timeout);
+
+                if (rep.Status == System.Net.NetworkInformation.IPStatus.Success)
+                {
+                    Console.WriteLine("{ 0} is up: ({ 1}ms)", ip, rep.RoundtripTime);
+                    nFound++;
+                }
+            }
+
+            stopWatch.Stop();
+            ts = stopWatch.Elapsed;
+
+            Console.WriteLine(nFound.ToString() + " devices found! Elapsed time: " + ts.ToString(), "Synchronous");
+        }
+
+        public async void RunPingSweep_Async()
+        {
+            nFound = 0;
+
+            var tasks = new List<Task>();
+
+            stopWatch.Start();
+
+            for (int i = StartIP; i <= StopIP; i++)
+            {
+                ip = BaseIP + i.ToString();
+
+                System.Net.NetworkInformation.Ping p = new System.Net.NetworkInformation.Ping();
+                var task = PingAndUpdateAsync(p, ip);
+                tasks.Add(task);
+            }
+
+            await Task.WhenAll(tasks).ContinueWith(t =>
+            {
+                stopWatch.Stop();
+                ts = stopWatch.Elapsed;
+                Console.WriteLine(nFound.ToString() + " devices found! Elapsed time: " + ts.ToString(), "Asynchronous");
+            });
+        }
+
+        private async Task PingAndUpdateAsync(System.Net.NetworkInformation.Ping ping, string ip)
+        {
+            var reply = await ping.SendPingAsync(ip, timeout);
+
+            if (reply.Status == System.Net.NetworkInformation.IPStatus.Success)
+            {
+                lock (lockObj)
+                { 
+                    nFound++;
+                }
+            }
+        }
+    }
+}
--- src/Workstation-Monitor/Program.cs
+++ src/Workstation-Monitor/Program.cs
@@ -5,55 +5,15 @@ using System.Threading;
 
 namespace Workstation_Monitor
 {
-    // Source: https://stackoverflow.com/a/4042887
-    // Original author: Tim Coker
-    //
     class Program
     {
-        static CountdownEvent countdown;
-        static int upCount = 0;
-        static object lockObj = new object();
-        const bool resolveNames = true;
+        
 
         static void Main(string[] args)
         {
-            countdown = new CountdownEvent(1);
-            Stopwatch sw = new Stopwatch();
-            sw.Start();
-            string ipBase = "192.168.1.";
-            for (int i = 1; i < 255; i++)
-            {
-                string ip = ipBase + i.ToString();
-
-                Ping p = new Ping();
-                p.PingCompleted += new PingCompletedEventHandler(p_PingCompleted);
-                countdown.AddCount();
-                p.SendAsync(ip, 100, ip);
-            }
-            countdown.Signal();
-            countdown.Wait();
-            sw.Stop();
-            TimeSpan span = new TimeSpan(sw.ElapsedTicks);
-            Console.WriteLine("Took {0} milliseconds. {1} hosts active.", sw.ElapsedMilliseconds, upCount);
+            PingSweep pingSweep = new PingSweep();
+            pingSweep.RunPingSweep_Async();
             Console.ReadLine();
-        }
-
-        static void p_PingCompleted(object sender, PingCompletedEventArgs e)
-        {
-            string ip = (string)e.UserState;
-            if (e.Reply != null && e.Reply.Status == IPStatus.Success)
-            {
-                Console.WriteLine("{0} is up: ({1} ms)", ip, e.Reply.RoundtripTime);
-                lock (lockObj)
-                {
-                    upCount++;
-                }
-            }
-            else if (e.Reply == null)
-            {
-                Console.WriteLine("Pinging {0} failed. (Null Reply object?)", ip);
-            }
-            countdown.Signal();
         }
     }
 }
